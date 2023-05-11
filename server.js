03c43008d2d0796bd129de14473a2e0996e55614
Anshav Parmar <126603892+Ansh7203@users.noreply.github.com>
2023-05-11 15:05:39 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-    res.send("My name is Anshav Parmar and currently I am pursuing Computer Programming Course.");
-    res.send("My hobbies are to play cricket and I love travelling with my loved ones.");
+    res.send("Anshav Parmar :- 167833219 ");
+
 });
 
 // setup http server to listen on HTTP_PORT
